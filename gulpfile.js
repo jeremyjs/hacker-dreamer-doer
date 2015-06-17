a9fb86fca0e895d6fca133e94d7a69239da98d01
Jeremy Meyer <jjman505@gmail.com>
2015-06-17 18:10:16 -0400

    added jshint watched task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
-/* File: gulpfile.js */
 
-// grab our gulp packages
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+// grab our packages
+var gulp   = require('gulp'),
+    jshint = require('gulp-jshint');
 
-// create a default task and just log a message
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+var sourcePaths = { js: 'source/js/**/*.js' };
+
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
+
+// configure the jshint task
+gulp.task('jshint', function() {
+  return gulp.src(sourcePaths.js)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+  gulp.watch(sourcePaths.js, ['jshint']);
 });
